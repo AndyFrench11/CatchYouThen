278d56e2c7acc5da324a792c788c371e8dfeeee6
andrew-french <andrew.french@xero.com>
2022-04-24 12:16:40 +1200

    Initial Commit

---
+++ CatchYouThen/CatchYouThenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchYouThenApp.swift
+//  CatchYouThen
+//
+//  Created by Andrew French on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CatchYouThenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
